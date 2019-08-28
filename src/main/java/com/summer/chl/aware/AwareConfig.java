2aff7bc4c65cb4a3857462dd4c66fd4dfa14cbd7
liu_dd <1034316694@qq.com>
2019-08-28 23:19:59 +0800

    spring boot的aware运用

---
+++ src/main/java/com/summer/chl/aware/AwareConfig.java
@@ -0,0 +1,9 @@
+package com.summer.chl.aware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.summer.chl.aware")
+public class AwareConfig {
+}
--- src/main/java/com/summer/chl/aware/AwareService.java
+++ src/main/java/com/summer/chl/aware/AwareService.java
@@ -1,4 +1,37 @@
 package com.summer.chl.aware;
 
-public class AwareService {
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class AwareService implements BeanNameAware, ResourceLoaderAware {
+    private String beanName;
+    private ResourceLoader loader;
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.loader = resourceLoader;
+    }
+
+    public void outputResult(){
+        System.out.println("bean 的名称为：" + beanName);
+        Resource resource = loader.getResource("classpath:com/summer/chl/aware/test.txt");
+        try {
+            String s = IOUtils.toString(resource.getInputStream());
+            System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
